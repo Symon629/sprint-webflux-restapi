cab7e6b6245618141a84f92864979b28c005d1ef
Symon <symonrahman20@gmail.com>
2024-06-04 16:46:11 +1000

    Create EmployeeDto and EmployeeMapper

--- src/main/java/com/symon/spring/webflux/dto/EmployeeDto.java
+++ src/main/java/com/symon/spring/webflux/dto/EmployeeDto.java
@@ -1,4 +1,17 @@
 package com.symon.spring.webflux.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDto {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
---
+++ src/main/java/com/symon/spring/webflux/mapper/EmployeeMapper.java
@@ -0,0 +1,23 @@
+package com.symon.spring.webflux.mapper;
+
+import com.symon.spring.webflux.dto.EmployeeDto;
+import com.symon.spring.webflux.entity.Employee;
+
+public class EmployeeMapper {
+    public static  EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+    public static  Employee maptoEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                emplemployeeDtooyee.getEmail()
+        );
+    }
+}
